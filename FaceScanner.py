c40c6cf32959fd9cdb2ff827e4d66f1fdc7d147c
Alex Stocks <stocksalex@icloud.com>
2020-08-13 10:11:11 +0100

    Finished FaceScanner.py

--- FaceScanner.py
+++ FaceScanner.py
@@ -7,99 +7,121 @@ from scapy.all import *
 # replace these with the location of your pictures and faces
 from scapy.layers.inet import TCP
 
-pictures_directory = "/home/alex/FaceScanner/pictures"
-faces_directory = "/home/alex/FaceScanner/faces"
+picturesDir = "/home/alex/FaceScanner/pictures"
+facesDir = "/home/alex/FaceScanner/faces"
 pcap_file = "packet.pcap"
 
+
 # takes raw HTTP traffic, splits headers
-def get_http_headers(http_payload):
-
+def httpHeaders(httpItem):
     try:
-        headers_raw = http_payload[:http_payload.index("\r\n\r\n") + 2]
+        rawHeaders = httpItem[:httpItem.index("\r\n\r\n") + 2]
 
-        headers = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\r\n", headers_raw))
-    except (TypeError):
+        headers = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\r\n", rawHeaders))
+    except TypeError:
         return None
     if "Content-Type" not in headers:
         return None
     return headers
+
+
 # take http headers, if image mime type in headers, split out type of image, if compressio then decompress
-def extract_image(headers, http_payload):
-    image = None
-    image_type = None
+def imageExtract(headers, httpItem):
+    newImage = None
+    newImage_type = None
 
     try:
         if "image" in headers['Content-Type']:
 
-            image_type = headers['Content-Type'].split("/")[1]
+            newImage_type = headers['Content-Type'].split("/")[1]
 
-            image = http_payload[http_payload.index("\r\n\r\n") + 4]
+            newImage = httpItem[httpItem.index("\r\n\r\n") + 4]
 
-            #if compression, decompress
+            # if compression, decompress
             try:
-                if"Content-Encoding" in headers.keys():
+                if "Content-Encoding" in headers.keys():
                     if headers['Content-Encoding'] == "gzip":
-                        image = zlib.decompress(image, 16+zlib.MAX_WBITS)
+                        newImage = zlib.decompress(newImage, 16 + zlib.MAX_WBITS)
                     elif headers['Content-Encoding'] == "deflate":
-                        image = zlib.decompress(image)
+                        newImage = zlib.decompress(newImage)
             except:
                 pass
     except:
         return None, None
-    return image, image_type
+    return newImage, newImage_type
 
 
-def http_assembler(pcap_file):
-    carved_images = 0
-    faces_detected = 0
+def detectFaces(path, file_name):
+    # reads image
+    img = cv2.imread(path)
+    # applies pre-trained classifier
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface.xml")
+    # returns rectangle coordinates for face in image
+    rects = cascade.detectMultiScale(img, 1.3, 4, cv2.CASCADE_SCALE_IMAGE, (20, 20))
+
+    if len(rects) == 0:
+        return False
+
+    rects[:, 2:] += rects[:, :2]
+    # draw triangle over area
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+    # write image 
+    cv2.imwrite("%s/%s-%s" % (facesDir, pcap_file, file_name), img)
+
+    return True
+
+
+def assmbleHttp(pcap_file):
+    foundImages = 0
+    detectedFaces = 0
 
     # open the pcap file for processing
-    a = rdpcap(pcap_file)
+    openPcap = rdpcap(pcap_file)
 
     # separate each tcp session into a dictionary
-    sessions = a.sessions()
+    sessionDict = openPcap.sessions()
 
-    for session in sessions:
-        http_payload = ""
+    for session in sessionDict:
+        httpItem = ""
 
-        for packet in sessions[session]:
+        for packet in sessionDict[session]:
 
             try:
                 if packet[TCP].dport == 80 or packet[TCP].sport == 80:
-
                     # concat only http traffic into single buffer
-                    http_payload += str(packet[TCP].payload)
+                    httpItem += str(packet[TCP].payload)
 
             except TypeError:
                 pass
-        headers = get_http_headers(http_payload)
+        headers = httpHeaders(httpItem)
 
         if headers is None:
             continue
 
-        image, image_type = extract_image(headers, http_payload)
+        image, image_type = imageExtract(headers, httpItem)
 
         if image is not None and image_type is not None:
 
-            file_name = "%s-FaceScanner_%d.%s" % (packet, carved_images, image_type)
+            file_name = "%s-FaceScanner_%d.%s" % (packet, foundImages, image_type)
 
-            fd = open("%s/%s" % (pictures_directory, file_name), "wb")
+            fd = open("%s/%s" % (picturesDir, file_name), "wb")
             fd.write(image)
             fd.close()
 
-            carved_images += 1
+            foundImages += 1
 
             try:
-                result = faces_detect("%s/%s" % (pictures_directory, file_name), file_name)
+                result = detectFaces("%s/%s" % (picturesDir, file_name), file_name)
 
                 if result is True:
-                    faces_detected += 1
+                    detectedFaces += 1
             except cv2.error:
                 pass
-    return carved_images, faces_detected
+    return foundImages, detectedFaces
 
-carved_images, faces_detected = http_assembler(pcap_file)
 
-print("Extracted: %d images" % carved_images)
-print("Detected: %d faces" % faces_detected)
+foundImages, detectedFaces = assmbleHttp(pcap_file)
 
+print("Extracted: %d images" % foundImages)
+print("Detected: %d faces" % detectedFaces)
